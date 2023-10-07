52c0daaab7977bbadb08e6df1d305be4fa821b75
AllanCicceri <allan.cicceri@gmail.com>
2023-10-07 19:08:51 -0300

    First commit

---
+++ src/main/java/com/allancicceri/univale/gerenciamento/requisitos/UnivaleGerenciamentoDeRequisitosApplication.java
@@ -0,0 +1,13 @@
+package com.allancicceri.univale.gerenciamento.requisitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivaleGerenciamentoDeRequisitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivaleGerenciamentoDeRequisitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allancicceri/univale/gerenciamento/requisitos/UnivaleGerenciamentoDeRequisitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allancicceri.univale.gerenciamento.requisitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivaleGerenciamentoDeRequisitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
